462af0aec8fb5ef93a07a0d5092cfac95cd4e1f1
O'Brian Santos <obrians@uci.edu>
2018-08-04 14:28:05 -0700

    Added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './Components/list/list.component';
+import { CreateComponent } from './Components/create/create.component';
+import { EditComponent } from './Components/edit/edit.component';
+
+import { MatToolbarModule } from '@angular/material';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'list',
+    component: ListComponent
+  },
+  { path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'edit/:id',
+    component: EditComponent
+  },
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
