2e23f1fc994e399652f46972107c8d0d4af6071d
O'Brian Santos <obrians@uci.edu>
2018-08-04 18:40:47 -0700

    Added service call to components

--- src/app/Components/create/create.component.ts
+++ src/app/Components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IssueService } from 'src/app/Service';
 
 @Component({
   selector: 'app-create',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private issueService: IssueService) { }
 
   ngOnInit() {
   }
--- src/app/Components/list/list.component.ts
+++ src/app/Components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IssueService } from 'src/app/Service';
 
 @Component({
   selector: 'app-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private issueService: IssueService) { }
 
   ngOnInit() {
   }
